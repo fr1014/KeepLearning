e622027800d57d6c4968ae06e3f77b21818c349f
fanrui07 <fanrui07@maoyan.com>
2022-02-11 10:43:30 +0800

    event

---
+++ app/src/main/java/com/fr1014/event/EventActivity.kt
@@ -0,0 +1,15 @@
+package com.fr1014.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fr1014.keeplearning.R
+
+class EventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event)
+    }
+
+
+}
